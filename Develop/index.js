89e7c793e9554512687c9ecd0cbaf623ebe160f6
Hannah Brownson <hannahbrownson18@gmail.com>
2022-07-30 17:09:17 -0400

    inquirer section

--- Develop/index.js
+++ Develop/index.js
@@ -15,3 +15,43 @@ init();
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "list",
+        message: "License?",
+        name: "license",
+        choices: ["Apache 2.0", "Boost", "IBM", "MIT", "WTFPL"],
+    },
+    {
+        type: "input",
+        message: "Description of project?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Do you have any installation instructions?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Usage?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Any contributors?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Tests run?",
+        name: "tests",
+    }
+    
+])
